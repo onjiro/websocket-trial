8412ec2f75d0b571ec32be32c65eda5da753daf0
onjiro_mohyahya <onjiro.mohyahya@gmail.com>
2011-06-16 23:22:03 +0900

    サーバー側を仮実装

--- jssrc/server.js
+++ jssrc/server.js
@@ -12,26 +12,59 @@ app.configure(function(){
 /**
  * websocket-server
  */
-var json = JSON.stringify;
+//var json = JSON.stringify;
 var server = ws.createServer({server: app});
- 
+var connections = [];
+
 server.addListener("listening", function(){
   sys.log("Listening for connections.");
 });
- 
+
 server.addListener("connection", function(conn){
-    
-    sys.log('Hello');
-    server.broadcast("@HELLO");
- 
+    // 接続時にコネクションにロールとidを割り振る。
+    // 最初に接続した人をHOSTとする。
+    // TODO id の生成を何らかのハッシュ値的なものに変更
+    connections.push(conn);
+    conn.send( connections.length === 1 ? 
+	       JSON.stringify({role: "HOST", id:"HOST"}):
+	       JSON.stringify({role: "GUEST", id:"GUEST" + (connections.length - 1)})
+	     );
+    sys.log('connected! number = ' + connections.length);
+
     conn.addListener("message", function(message){
-//	server.broadcast(message);
-	conn.send(message);
+	if (conn === connections[0]) {
+	    // HOSTからのメッセージはbroadcastする。
+	    server.broadcast(message);
+	} else {
+	    // GUESTからのメッセージはHOSTのみに送信
+	    connections[0].send(message);
+	}
+    });
+
+    conn.addListener("close", function() {
+	if (connections[0] === conn) {
+	    // HOSTが切断したら全員切断する。
+	    sys.log('HOST has disconnected!');
+	    connections[0].send(JSON.stringify({action:"close", key:"HOST"}));
+	    for (var i = 1; i < connections.length; i++) {
+		connections[i].close();
+	    }
+	    connections = [];
+	} else {
+	    // GUESTが切断した場合はHOSTに切断した旨を送信。
+	    for (var i = 0; i < connections.length; i++) {
+		if (connections[i] === conn) {
+		    connections[0].send(JSON.stringify({action:"close", key:"GUEST" + i}));
+		    connections.splice(i, 1);
+		}
+	    }
+	}
     });
     
 });
  
 server.addListener("close", function(conn){
+    sys.log('closed');
     //server.broadcast("@BYE");
 });
  
